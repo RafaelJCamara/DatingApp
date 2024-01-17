ac0ca944dd0f5fea81f30d4b8876b1554bc90276
RafaelJCamara <rafaelcamarac@gmail.com>
2024-01-17 11:29:04 +0100

    [client] Add error handling.

--- API/DatingApp.API/DTOs/LoginDto.cs
+++ API/DatingApp.API/DTOs/LoginDto.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs;
 
 public class LoginDto
 {
+    [Required]
     public string? Username { get; set; }
+    
+    [Required]
     public string? Password { get; set; }
 }
