b29e6bee1ee4597b32994d47876fcdb3ccd3944b
RafaelJCamara <rafaelcamarac@gmail.com>
2024-01-25 19:14:23 +0100

    [client] Add support to update member.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,7 +24,11 @@ const routes: Routes = [
         canActivate: [AuthGuard],
       },
       { path: 'members/:username', component: MemberDetailComponent },
-      { path: 'member/edit', component: MemberEditComponent },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        canDeactivate: [preventUnsavedChangesGuard],
+      },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
