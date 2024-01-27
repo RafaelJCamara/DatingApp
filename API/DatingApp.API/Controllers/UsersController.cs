7fd237b918941f6be3c28a8d71551b65f8dbcf14
RafaelJCamara <rafaelcamarac@gmail.com>
2024-01-27 21:49:39 +0100

    [API] Add delete photo.

--- API/DatingApp.API/Controllers/UsersController.cs
+++ API/DatingApp.API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using DatingApp.API.Entities;
 using DatingApp.API.Extensions;
 using DatingApp.API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -95,6 +96,30 @@ namespace DatingApp.API.Controllers
             if (await _userRepository.SaveAllAsync()) return NoContent();
             
             return BadRequest("Problem setting the main photo");
+        }
+        
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("You can't delete your main photo");
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+            
+            return BadRequest("Problem deleting photo");
         }
         
     }
