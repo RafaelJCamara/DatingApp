9865c72a07aa200e76146e9eb86994b3cf3b4b02
RafaelJCamara <rafaelcamarac@gmail.com>
2024-02-02 17:13:57 +0100

    [API] Extender user registration model.

--- API/DatingApp.API/Controllers/AccountController.cs
+++ API/DatingApp.API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using System.Security.Cryptography;
 using System.Text;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.DTOs;
 using DatingApp.API.Entities;
@@ -15,11 +16,13 @@ public class AccountController : ControllerBase
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(DataContext context, ITokenService tokenService)
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
         _context = context;
         _tokenService = tokenService;
+        _mapper = mapper;
     }
 
     [HttpPost("register")]
@@ -27,23 +30,23 @@ public class AccountController : ControllerBase
     {
         if (await _context.Users.AnyAsync(user => user.UserName.Equals(registerDto.Username.ToLower())))
             return BadRequest("User is already taken");
+
+        var user = _mapper.Map<AppUser>(registerDto);
         
         using var hmac = new HMACSHA512();
 
-        var user = new AppUser
-        {
-            UserName = registerDto.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
-        };
-
+        user.UserName = registerDto.Username.ToLower();
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+        user.PasswordSalt = hmac.Key;
+        
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
         };
     }
     
@@ -69,7 +72,8 @@ public class AccountController : ControllerBase
         {
             Username = user.UserName,
             Token = _tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            KnownAs = user.KnownAs
         };
     }
 }
