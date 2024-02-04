96149dc10999b2a8ce1a5167401122bd54df8ae6
RafaelJCamara <rafaelcamarac@gmail.com>
2024-02-04 12:13:22 +0100

    [client] Add pagination.

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { map, of } from 'rxjs';
+import { PaginatedResult } from '../_models/pagination';
 
 @Injectable({
   providedIn: 'root',
@@ -10,19 +11,36 @@ import { map, of } from 'rxjs';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  pagintedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
 
   constructor(private http: HttpClient) {}
 
-  getMembers() {
-    //we are returning an observable
-    if (this.members.length > 0) return of(this.members);
+  getMembers(page?: number, itemsPerPage?: number) {
+    // //we are returning an observable
+    // if (this.members.length > 0) return of(this.members);
 
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map((members) => {
-        this.members = members;
-        return members;
-      })
-    );
+    let params = new HttpParams(); //this is an utility class from Angular that allow us to set query strings parameters
+
+    if (page && itemsPerPage) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http
+      .get<Member[]>(this.baseUrl + 'users', { observe: 'response', params }) //to acess the headers, we must add this observe
+      .pipe(
+        map((response) => {
+          if (response.body) {
+            this.pagintedResult.result = response.body;
+          }
+          const pagination = response.headers.get('Pagination');
+          if (pagination) {
+            this.pagintedResult.pagination = JSON.parse(pagination);
+          }
+
+          return this.pagintedResult;
+        })
+      );
   }
 
   getMember(username: string) {
