db4b8fea8da4ca5769b973d1c5e831f2fa8c8c8b
Rafael Câmara <rafaelcamarac@gmail.com>
2024-02-23 15:48:28 +0000

    [client] Add read and uread icons.

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -4,13 +4,21 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { TimeagoModule } from 'ngx-timeago';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
-  imports: [CommonModule, TimeagoModule, FormsModule],
+  imports: [
+    CommonModule,
+    TimeagoModule,
+    FormsModule,
+    MatTooltipModule,
+    MatButtonModule,
+  ],
 })
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm;
@@ -34,5 +42,21 @@ export class MemberMessagesComponent implements OnInit {
           this.messageForm?.reset();
         },
       });
+  }
+
+  computeMessageStatus(dateRead: Date | undefined, sendUsername: string) {
+    if (this.isMessageUnread(dateRead, sendUsername)) return 'unread';
+
+    if (this.isMessageRead(dateRead, sendUsername)) return 'read ';
+
+    return;
+  }
+
+  isMessageUnread(dateRead: Date | undefined, sendUsername: string) {
+    return !dateRead && sendUsername !== this.username;
+  }
+
+  isMessageRead(dateRead: Date | undefined, sendUsername: string) {
+    return dateRead && sendUsername !== this.username;
   }
 }
