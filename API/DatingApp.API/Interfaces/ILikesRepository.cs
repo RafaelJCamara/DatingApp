a46f83f7b048176dc035d1da74552ebe7edc4677
Rafael Câmara <rafaelcamarac@gmail.com>
2024-03-10 15:58:50 +0000

    [API] Improve getting likes for user.

--- API/DatingApp.API/Interfaces/ILikesRepository.cs
+++ API/DatingApp.API/Interfaces/ILikesRepository.cs
@@ -7,6 +7,7 @@ namespace DatingApp.API.Interfaces;
 public interface ILikesRepository
 {
     Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+    Task<bool> DoesCurrentUserLikeTargetUser(int currentUserId, int targetUserId);
     Task<AppUser> GetUserWithLikes(int userId);
     Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
 }
