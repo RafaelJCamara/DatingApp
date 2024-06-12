fba4830487fd96c936410d8b7f5709e734f6137d
Rafael Camara <rafaelcamarac@gmail.com>
2024-06-12 10:41:53 +0200

    add tracing

--- API/DatingApp.Infrastructure/Events/Handlers/EmailValidatedEventHandler.cs
+++ API/DatingApp.Infrastructure/Events/Handlers/EmailValidatedEventHandler.cs
@@ -1,4 +1,5 @@
-﻿using DatingApp.Common.Events;
+﻿using System.Diagnostics;
+using DatingApp.Common.Events;
 using DatingApp.Domain.Models;
 using MassTransit;
 using Microsoft.AspNetCore.Identity;
@@ -17,14 +18,22 @@ namespace DatingApp.Infrastructure.Events.Handlers
 
         public async Task Consume(ConsumeContext<EmailValidatedEvent> context)
         {
-            var user = await _userManager
+            var activitySource = new ActivitySource("DatingApp");
+            using (var activity = activitySource.StartActivity("[Consumer] Email validated event", ActivityKind.Consumer))
+            {
+                activity?.SetTag("messaging.system", "rabbitmq");
+                activity?.SetTag("messaging.origin_kind", "queue");
+                activity?.SetTag("messaging.rabbitmq.queue", "email-validated-event");
+                
+                var user = await _userManager
                     .Users
                     .SingleOrDefaultAsync(x => x.UserName.Equals(context.Message.Username) && x.Email.Equals(context.Message.Email));
 
-            if(user is not null)
-            {
-                user.EmailConfirmed = true;
-                await _userManager.UpdateAsync(user);
+                if(user is not null)
+                {
+                    user.EmailConfirmed = true;
+                    await _userManager.UpdateAsync(user);
+                }   
             }
         }
     }
